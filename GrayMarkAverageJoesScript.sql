63cea01199eb9c674a4734becfa3d07e508bda8a
markagray1 <markagray1@gmail.com>
2024-08-09 13:02:28 -0500

    Update GrayMarkAverageJoesScript.sql

--- GrayMarkAverageJoesScript.sql
+++ GrayMarkAverageJoesScript.sql
@@ -1,84 +1,77 @@
-use #Placeholder
-go
+USE #Placeholder
+GO
 
-drop view if exists ViewProductList
-go
+DROP VIEW IF EXISTS ViewProductList
+GO
 
-Drop Table if exists Product
-Drop Table if exists ProductType
-drop table if exists ProductSize
-Drop table if exists ProductColor
-go
+DROP TABLE IF EXISTS Product
+DROP TABLE IF EXISTS ProductType
+DROP TABLE IF EXISTS ProductSize
+DROP TABLE IF EXISTS ProductColor
+GO
 
---create view viewProduct
---as
---select ProductID, ProductName, ProductSize, ProductColor, ProductQuality, ProductPrice, ImagePath
---from product p join ProductType pt on p.ProductID = pt.ProductTypeID
---go
-
-create Table ProductType 
+CREATE TABLE ProductType 
 (
-	ProductTypeID int Identity(1,1) Constraint pkProductTypeID Primary Key not null,
-	ProductTypeName varchar(25) not null
+	ProductTypeID int IDENTITY(1,1) CONSTRAINT pkProductTypeID PRIMARY KEY NOT NULL,
+	ProductTypeName varchar(25) NOT NULL
 )
 
-create Table ProductSize
+CREATE TABLE ProductSize
 (
-	ProductSizeID int Identity(1,1) Constraint pkProductSizeID Primary Key not null,
-	ProductSizeName varchar(25) not null
+	ProductSizeID int IDENTITY(1,1) CONSTRAINT pkProductSizeID PRIMARY KEY NOT NULL,
+	ProductSizeName varchar(25) NOT NULL
 )
 
-create table ProductColor
+CREATE TABLE ProductColor
 (
-	ProductColorID int Identity(1,1) Constraint pkProductColorID Primary Key not null,
-	ProductColorName varchar(25) not null
+	ProductColorID int IDENTITY(1,1) Constraint pkProductColorID PRIMARY KEY NOT NULL,
+	ProductColorName varchar(25) NOT NULL
 )
 
-create Table Product
+CREATE TABLE Product
 (
-	ProductID int Identity(1,1) Constraint pkProductID Primary Key not null,
-	ProductName varchar(100) not null,
-	ProductDescription varchar(200) null,
-	ProductPrice decimal(4,2) not null,
-	ImagePath varchar(200) not null,
-	ProductColorID int not null Constraint fkProductColorID Foreign Key references ProductColor(ProductColorID),
-	ProductSizeID int not null Constraint fkProductSizeID Foreign Key references ProductSize(ProductSizeID),
-	ProductTypeID int not null Constraint fkProductTypeID Foreign Key references ProductType(ProductTypeID)
+	ProductID int IDENTITY(1,1) CONSTRAINT pkProductID PRIMARY KEY NOT NULL,
+	ProductName varchar(100) NOT NULL,
+	ProductDescription varchar(200) NULL,
+	ProductPrice decimal(4,2) NOT NULL,
+	ImagePath varchar(200) NOT NULL,
+	ProductColorID int NOT NULL CONSTRAINT fkProductColorID FOREIGN KEY REFERENCES ProductColor(ProductColorID),
+	ProductSizeID int NOT NULL CONSTRAINT fkProductSizeID FOREIGN KEY REFERENCES ProductSize(ProductSizeID),
+	ProductTypeID int NOT NULL CONSTRAINT fkProductTypeID FOREIGN KEY REFERENCES ProductType(ProductTypeID)
 )
 
-Insert Into ProductType (ProductTypeName)
-values ('Jersey'), ('Jersey Shorts'), ('Headband'), ('Wristbands'), ('Short-sleeve'), ('Long-sleeve'), ('Tank Top'), ('Sweatshirt'), ('Shorts'), ('Hat') 
+INSERT INTO ProductType (ProductTypeName)
+VALUES ('Jersey'), ('Jersey Shorts'), ('Headband'), ('Wristbands'), ('Short-sleeve'), ('Long-sleeve'), ('Tank Top'), ('Sweatshirt'), ('Shorts'), ('Hat') 
 
-insert into ProductSize (ProductSizeName)
-values ('Small'), ('Medium'), ('Large')
+INSERT INTO ProductSize (ProductSizeName)
+VALUES ('Small'), ('Medium'), ('Large')
 
-insert into ProductColor (ProductColorName)
-values ('Yellow'), ('White'), ('Gray')
+INSERT INTO ProductColor (ProductColorName)
+VALUES ('Yellow'), ('White'), ('Gray')
 
 INSERT INTO Product (ProductName, ProductDescription, ProductPrice, ImagePath, ProductTypeID, ProductSizeID, ProductColorID)
 VALUES 
-('Average Joe''s Jersey', null, 39.99, '/Images/Jersey.jpg', 1, 2, 1),
-('Average Joe''s Jersey Shorts', null, 29.99, '/Images/Jersey Shorts.jpg',2, 1, 3),
-('Average Joe''s Headband', null, 9.99, '/Images/Headband.jpg',3, 1,2),
-('Average Joe''s Wristbands', null, 4.99, '/Images/Wristbands.jpg',4,3,3),
-('Average Joe''s Short-Sleeve', null, 19.99, '/Images/Short-Sleeve.jpg', 5,1,2),
-('Average Joe''s Long-Sleeve', null, 19.99, '/Images/Long-Sleeve.jpg', 6,2,1),
-('Average Joe''s Tank Top', null, 19.99,  '/Images/Tank Top.jpg',7,1,3),
-('Average Joe''s Sweatshirt', null, 29.99,  '/Images/Sweatshirt.jpg',8,3,2),
-('Average Joe''s Shorts', null, 19.99,  '/Images/Shorts.jpg',9,1,1),
-('Average Joe''s Hat', null, 14.99,  '/Images/Hat.jpg',10,2,2)
+('Average Joe''s Jersey', NULL, 39.99, '/Images/Jersey.jpg', 1, 2, 1),
+('Average Joe''s Jersey Shorts', NULL, 29.99, '/Images/Jersey Shorts.jpg',2, 1, 3),
+('Average Joe''s Headband', NULL, 9.99, '/Images/Headband.jpg',3, 1,2),
+('Average Joe''s Wristbands', NULL, 4.99, '/Images/Wristbands.jpg',4,3,3),
+('Average Joe''s Short-Sleeve', NULL, 19.99, '/Images/Short-Sleeve.jpg', 5,1,2),
+('Average Joe''s Long-Sleeve', NULL, 19.99, '/Images/Long-Sleeve.jpg', 6,2,1),
+('Average Joe''s Tank Top', NULL, 19.99,  '/Images/Tank Top.jpg',7,1,3),
+('Average Joe''s Sweatshirt', NULL, 29.99,  '/Images/Sweatshirt.jpg',8,3,2),
+('Average Joe''s Shorts', NULL, 19.99,  '/Images/Shorts.jpg',9,1,1),
+('Average Joe''s Hat', NULL, 14.99,  '/Images/Hat.jpg',10,2,2)
 
-go
-create view ViewProductList
-as
+GO
+CREATE view ViewProductList
+AS
 SELECT p.ProductID, tp.ProductTypeName, st.ProductSizeName, ct.ProductColorName, p.ProductName, p.ProductDescription, p.ProductPrice, p.ImagePath, tp.ProductTypeID, st.ProductSizeID, ct.ProductColorID
 FROM Product p
 INNER JOIN ProductType tp ON p.ProductTypeID = tp.ProductTypeID
-inner join ProductSize st on p.ProductSizeID = st.ProductSizeID
-inner join ProductColor ct on p.ProductColorID = ct.ProductColorID;
-go
+INNER JOIN ProductSize st ON p.ProductSizeID = st.ProductSizeID
+INNER JOIN ProductColor ct ON p.ProductColorID = ct.ProductColorID;
+GO
 
---select * from ViewProductList where ProductTypeID = 1;
 
 
 
